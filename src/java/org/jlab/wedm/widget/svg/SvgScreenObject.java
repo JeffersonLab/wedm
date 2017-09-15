25231d608b82d356f4096d461184116928a14e4a
slominskir <Ryan.Slominski@gmail.com>
2017-09-15 17:50:18 -0400

    Work around EDM oddity with zero width rect with zero width line

--- src/java/org/jlab/wedm/widget/svg/SvgScreenObject.java
+++ src/java/org/jlab/wedm/widget/svg/SvgScreenObject.java
@@ -13,12 +13,29 @@ public abstract class SvgScreenObject extends CoreWidget {
     
     public String startSvg(String indent, Point translation) {
         
+        // EDM draws a line even if object has 0 width and 0 line thickness.  This is madness.
+        // Further, the box model is likely different too:
+        // WEDM uses BORDER-BOX style for HTML elements (border included in dimensions)
+        // and for SVG elements border: is placed in middle of dimensions (half of it is inside, half outside).
+        // EDM seems to be widget-specific 
+        // For HTML we could use CONTENT-BOX (border is drawn outside dimensions completely).
+        // For SVG we could try to control box model / stroke location to be like CONTENT-BOX or BORDER-BOX, but it isn't easy:
+        // https://stackoverflow.com/questions/7241393/can-you-control-how-an-svgs-stroke-width-is-drawn
+        
         // EDM Oddity
         if (lineWidth != null) {
             if (lineWidth < 1.0) {
                 lineWidth = 1.0f; // This is due to EDM rendering 0 width border as 1+
             }
         }     
+        
+        // EDM Oddity
+        if(w == 0) {
+            w = 1; // This is due to EDM rendering 0 width rect with 0 size border as 1
+        }
+        if(h == 0) {
+            h = 1;
+        }
         
         this.setCommonAttributes();       
 
