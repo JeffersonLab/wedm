4de67a94610df175bd13240f507a88080fbbc7ab
slominskir <Ryan.Slominski@gmail.com>
2018-01-09 14:55:56 -0500

    Initial support for RegTextupdateClass widget

---
+++ src/java/org/jlab/wedm/widget/html/ActiveRegExUpdateText.java
@@ -0,0 +1,9 @@
+package org.jlab.wedm.widget.html;
+
+/**
+ *
+ * @author ryans
+ */
+public class ActiveRegExUpdateText extends ActiveUpdateText {
+    
+}
---
+++ web/resources/widgets/RegTextupdateClass/widget.js
@@ -0,0 +1,4 @@
+jlab = jlab || {};
+jlab.wedm = jlab.wedm || {};
+jlab.wedm.classToObserverMap = jlab.wedm.classToObserverMap || {};
+jlab.wedm.classToObserverMap['ActiveRegExUpdateText'] = 'jlab.wedm.ControlTextPvObserver';
