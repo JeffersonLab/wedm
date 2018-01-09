91711b08e24e3dc033266ba5bb592cd9553fe8f8
slominskir <Ryan.Slominski@gmail.com>
2018-01-09 15:27:12 -0500

    Set default precision for scientific notation

--- src/java/org/jlab/wedm/widget/html/ActiveUpdateText.java
+++ src/java/org/jlab/wedm/widget/html/ActiveUpdateText.java
@@ -8,16 +8,22 @@ import org.jlab.wedm.persistence.model.ColorPalette;
  * @author ryans
  */
 public class ActiveUpdateText extends ActiveControlText {
-    public String displayMode = "default";    
-    
+
+    public String displayMode = "default";
+
     @Override
     public void parseTraits(Map<String, String> traits, ColorPalette colorList) {
         super.parseTraits(traits, colorList);
-        
+
         displayMode = traits.get("displayMode");
-        
-        if("exp".equals(displayMode) || "engineer".equals(displayMode)) {
+
+        if ("exp".equals(displayMode) || "engineer".equals(displayMode)) {
             format = "exponential";
+
+            // Special case: We explicity default precision to zero if not defined (but only if scientific notation!)
+            if (precision == null) {
+                precision = 0;
+            }
         }
-    }    
+    }
 }
