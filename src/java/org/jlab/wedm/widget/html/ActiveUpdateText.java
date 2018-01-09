84db0471d683d54b7db4bf7212e5ef1fdaac5847
slominskir <Ryan.Slominski@gmail.com>
2018-01-09 14:55:10 -0500

    Handle scientific notation

--- src/java/org/jlab/wedm/widget/html/ActiveUpdateText.java
+++ src/java/org/jlab/wedm/widget/html/ActiveUpdateText.java
@@ -1,9 +1,23 @@
 package org.jlab.wedm.widget.html;
 
+import java.util.Map;
+import org.jlab.wedm.persistence.model.ColorPalette;
+
 /**
  *
  * @author ryans
  */
 public class ActiveUpdateText extends ActiveControlText {
+    public String displayMode = "default";    
     
+    @Override
+    public void parseTraits(Map<String, String> traits, ColorPalette colorList) {
+        super.parseTraits(traits, colorList);
+        
+        displayMode = traits.get("displayMode");
+        
+        if("exp".equals(displayMode) || "engineer".equals(displayMode)) {
+            format = "exponential";
+        }
+    }    
 }
