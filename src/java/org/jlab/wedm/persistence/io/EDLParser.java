0d4275c0eb5ed1874f11acddf330a24a2d1be516
slominskir <Ryan.Slominski@gmail.com>
2019-02-13 12:14:41 -0500

    formatting updated only

--- src/java/org/jlab/wedm/persistence/io/EDLParser.java
+++ src/java/org/jlab/wedm/persistence/io/EDLParser.java
@@ -33,11 +33,10 @@ public class EDLParser {
     public static final String HTTP_DOC_ROOT;
 
     /**
-     * On Windows you could set EDL_DIR to a remote ExpanDrive mount say 
-     * E:\cs\opshome\edm then set REWRITE_FROM_DIR to / 
-     * and REWRITE_TO_DIR to E:\.
-     **/
-    
+     * On Windows you could set EDL_DIR to a remote ExpanDrive mount say
+     * E:\cs\opshome\edm then set REWRITE_FROM_DIR to / and REWRITE_TO_DIR to
+     * E:\.
+     */
     static {
         final String defaultRoot = "C:\\EDL";
         String root = System.getenv("EDL_DIR");
@@ -49,60 +48,56 @@ public class EDLParser {
 
         REWRITE_FROM_DIR = System.getenv("REWRITE_FROM_DIR");
         REWRITE_TO_DIR = System.getenv("REWRITE_TO_DIR");
-        
+
         // Use search path?
         String search_path = System.getenv("EDMDATAFILES");
-        if (search_path != null)
-        {
+        if (search_path != null) {
             SEARCH_PATH = search_path.split(":");
             LOGGER.log(Level.INFO, "EDMDATAFILES search path:\n{0}", Arrays.toString(SEARCH_PATH));
-        }
-        else
+        } else {
             SEARCH_PATH = null;
+        }
 
         // .. search path with http/https as starting point?
         HTTP_DOC_ROOT = System.getenv("EDMHTTPDOCROOT");
-        if (HTTP_DOC_ROOT != null)
-        {
+        if (HTTP_DOC_ROOT != null) {
             LOGGER.log(Level.INFO, "EDMHTTPDOCROOT: {0}", HTTP_DOC_ROOT);
-            if (SEARCH_PATH == null)
+            if (SEARCH_PATH == null) {
                 LOGGER.log(Level.WARNING, "EDMHTTPDOCROOT must be used with EDMDATAFILES");
+            }
 
             // If http:.. access is configured,
             // local files are often served with self-signed cert,
             // so disable validation
-            try
-            {
+            try {
                 trustAnybody();
-            }
-            catch (Exception ex)
-            {
+            } catch (Exception ex) {
                 LOGGER.log(Level.WARNING, "Cannot disable certificate checks", ex);
             }
         }
     }
 
-    /** Allow https:// access to self-signed certificates
-     *  @throws Exception on error
+    /**
+     * Allow https:// access to self-signed certificates
+     *
+     * @throws Exception on error
      */
-    private static synchronized void trustAnybody() throws Exception
-    {
+    private static synchronized void trustAnybody() throws Exception {
         // Create a trust manager that does not validate certificate chains.
-        final TrustManager[] trustAllCerts = new TrustManager[]
-        {
-            new X509TrustManager()
-            {
+        final TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
                 @Override
-                public java.security.cert.X509Certificate[] getAcceptedIssuers()
-                {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
                     return null;
                 }
+
                 @Override
-                public void checkClientTrusted(X509Certificate[] certs, String authType)
-                { /* NOP */ }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                    /* NOP */ }
+
                 @Override
-                public void checkServerTrusted(X509Certificate[] certs, String authType)
-                { /* NOP */ }
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                    /* NOP */ }
             }
         };
         final SSLContext sc = SSLContext.getInstance("SSL");
@@ -123,7 +118,7 @@ public class EDLParser {
                 name = REWRITE_TO_DIR + name;
             }
         }
-        
+
         return name;
     }
 
@@ -147,20 +142,20 @@ public class EDLParser {
         return edl;
     }
 
-    /** Resolve name to URL
-     * 
-     *  @param name Name may be a http:, https:, file: URL,
-     *              but also just a name that refers to a local file,
-     *              or that is found on the EDMDATAFILES search path.
-     *  @return Resolved URL for the name or <code>null</code>
-     *  @throws MalformedURLException
+    /**
+     * Resolve name to URL
+     *
+     * @param name Name may be a http:, https:, file: URL, but also just a name
+     * that refers to a local file, or that is found on the EDMDATAFILES search
+     * path.
+     * @return Resolved URL for the name or <code>null</code>
+     * @throws MalformedURLException
      */
     public static URL getEdlURL(String name) throws MalformedURLException {
         Objects.requireNonNull(name, "An EDL resource is required");
 
         // Use complete http.. URL as is
-        if (name.startsWith("http:")  ||  name.startsWith("https:"))
-        {
+        if (name.startsWith("http:") || name.startsWith("https:")) {
             LOGGER.log(Level.FINE, "Using {0} as provided", name);
             return new URL(name);
         }
@@ -168,14 +163,14 @@ public class EDLParser {
         final File edl_file;
         // For file URL, get the file.
         // Otherwise turn name into absolute filename within EDL_ROOT_DIR
-        if (name.startsWith("file:"))
+        if (name.startsWith("file:")) {
             edl_file = new File(URI.create(name));
-        else
+        } else {
             edl_file = getEdlFile(name);
+        }
 
         // Done?
-        if (edl_file.exists())
-        {
+        if (edl_file.exists()) {
             LOGGER.log(Level.FINE, "Found local file {0}", edl_file);
             return edl_file.toURI().toURL();
         }
@@ -184,39 +179,34 @@ public class EDLParser {
         URL edl = null;
 
         // Assert that name has *.edl ending
-        if (!name.contains(".edl"))
-        {
+        if (!name.contains(".edl")) {
             // The file extension should precede any macro following the resource name
             final int idx = name.indexOf("&");
-            if (idx != -1)
+            if (idx != -1) {
                 name = name.substring(0, idx) + ".edl" + name.substring(idx);
-            else
+            } else {
                 name += ".edl";
+            }
         }
 
-        if (SEARCH_PATH != null)
-        {
-            for (String path : SEARCH_PATH)
-            {
-                if (path.startsWith("/"))
+        if (SEARCH_PATH != null) {
+            for (String path : SEARCH_PATH) {
+                if (path.startsWith("/")) {
                     edl = new URL(HTTP_DOC_ROOT + path.substring(1) + "/" + name);
-                else
+                } else {
                     edl = new URL(HTTP_DOC_ROOT + path + "/" + name);
+                }
                 LOGGER.log(Level.FINER, "Checking {0}", edl);
-                try
-                {
+                try {
                     // Perform HEAD request to check for presence
                     final HttpURLConnection edl_conn = (HttpURLConnection) edl.openConnection();
                     edl_conn.setRequestMethod("HEAD");
                     final int code = edl_conn.getResponseCode();
-                    if (code == 200)
-                    {
+                    if (code == 200) {
                         LOGGER.log(Level.FINE, "File found at {0}", edl);
                         return edl;
                     }
-                }
-                catch (Exception ex)
-                {
+                } catch (Exception ex) {
                     LOGGER.log(Level.FINER, "File not found at " + path, ex);
                 }
             }
